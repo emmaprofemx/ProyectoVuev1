2263272646072cc6c1b1433866cf47f473edd5f3
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-04-16 12:30:54 -0600

    Agregando icono carrito

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from "vue";
 import App from "./App.vue";
 import "bootstrap/dist/js/bootstrap.js"
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 createApp(App).mount('#app')
