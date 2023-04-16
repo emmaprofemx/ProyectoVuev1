a3f4c4e6961d0fc8b60754ad8e13d0a8f857b04d
Emmanuel Alejandro Ruiz Garcia <emmaprofemx@gmail.com>
2023-04-16 02:17:52 -0600

    INSTALACION DE BOOSTRAP HECHA

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <to-do-item></to-do-item>
+  <ul>
+  </ul>
+  <h1>To-Do List</h1>
+</div>
+
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDoItem from "./components/ToDoItem.vue";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDoItem,
   }
 }
 </script>
